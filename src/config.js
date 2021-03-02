c320629e71c383defa643d5953f976cc92b51e2f
karlaluuu <karlamari13@gmail.com>
2021-03-02 03:14:30 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const production = true; // set it to true when deploy to the server
 
-const domain = `educloud-mern.herokuapp.com`; // if you have domain pointed to digitalOcean Cloud server let use your domain.eg: tabvn.com
+const domain = `production ? '139.59.227.127' : '127.0.0.1:3001'`; // if you have domain pointed to digitalOcean Cloud server let use your domain.eg: tabvn.com
 export const websocketUrl = `ws://${domain}`
 export const apiUrl = `https://${domain}`
